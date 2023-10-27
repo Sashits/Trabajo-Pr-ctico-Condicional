8ae201d2fadaf6bb1fa1e5f8ab5bb3905e67cedf
alumno <49481966@ivt.edu.ar>
2023-10-27 09:31:39 -0300

    clase js

---
+++ tp.js
@@ -0,0 +1,13 @@
+function edadlegal
+
+let legal = prompt('Ingresa tu edad')
+{
+    
+    if (edad >=18)
+    alert ('Podes ir al boliche') 
+    else
+    {
+     alert ('No podes ir l boliche')
+    }
+}
+edadlegal (edad >=18)
