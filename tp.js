afc17deac6d755b627a5a639c90196cf2a204880
alumno <49481966@ivt.edu.ar>
2023-11-10 09:29:55 -0300

    botones

--- tp.js
+++ tp.js
@@ -1,2 +1,20 @@
-let boton = document.querySelector ('#cambiarimagen')
- 
+let botonRojo = document.querySelector('#botonRojo')
+let botonAzul = document.querySelector('#botonAzul')
+let botonVerde = document.querySelector('#botonVerde ')
+
+botonRojo.onclick = function() {
+    titulo.style.color = 'red'
+} 
+
+botonAzul.onclick = function() {
+    titulo.style.color = 'blue'
+} 
+
+
+let tamaño = document.querySelector('img  ')
+
+let aumento = 100
+tamaño.onclick = function(){
+    aumento = aumento + 10
+    imagen1.style.width = aumento + 'px'
+}
